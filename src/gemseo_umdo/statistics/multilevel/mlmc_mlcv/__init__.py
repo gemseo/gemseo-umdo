1a81445e91bd408e6fbde4f47c6e1548fe3916b0
Matthias De Lozzo <matthias.delozzo@irt-saintexupery.com>
2023-08-07 12:09:17 +0000

    Write User Guide + Developer Guide (first bricks) + Getting started

--- src/gemseo_umdo/statistics/multilevel/mlmc_mlcv/__init__.py
+++ src/gemseo_umdo/statistics/multilevel/mlmc_mlcv/__init__.py
@@ -12,5 +12,5 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-r"""Multi-level Monte Carlo with multi-level control variate (MLMC-MLCV) algorithm."""
+r"""Multilevel Monte Carlo with multilevel control variate (MLMC-MLCV) algorithm."""
 from __future__ import annotations
