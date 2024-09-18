61985e9d2c7fb11310b3157bb5d1dd189ca5d92a
AntoineD <>
2024-09-18 10:15:01 +0000

    Deployed a293697 to develop in public with MkDocs 1.6.1 and mike 2.1.3

---
+++ public/develop/generated/examples/umdo/control_variate/cv_sellar.py
@@ -0,0 +1,128 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This work is licensed under a BSD 0-Clause License.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+# FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+r"""# The Sellar's MDO problem
+
+In this example,
+we consider the Sellar's MDO problem under uncertainty
+
+$$\min_{x,z_1,x_2} \mathbb{E}[f(x,z_2,y_1,y_2)]$$
+
+over the design space $[0,10]\times[-10,10]\times[0,10]$ and
+under the inequality constraints
+
+$$\mathbb{E}[c_1(y_1)]+3\mathbb{S}[c_1(y_1)] \leq 0$$
+
+and
+
+$$\mathbb{E}[c_2(y_2)]+3\mathbb{S}[c_2(y_2)] \leq 0,$$
+
+where
+
+- $\mathbb{E}$ is the expectation operator,
+- $\mathbb{S}$ is the standard deviation operators,
+- $f(x,z_2) = x^2 + z_2 + y_1^2 + \exp(-y_2)$ is the objective function,
+- $c_1(y_1) = 3.16 - y_1^2$ is the first constraint function,
+- $c_2(y_2) = y_2 - 24.0$ is the second constraint function,
+- $y_1 = \sqrt{z_1^2 + z_2 + x - ay_2}$ is the first coupling equation,
+- $y_2 = \frac{\log(1+\exp(10y_1))}{5} - y_1 - \frac{\log(2)}{5} + z_1 + z_2$
+  is the second coupling equation,
+- $a$ is a random variable distributed
+  according to the triangular distribution $\mathcal{T}(0.1,0.2,0.3)$.
+"""
+
+from __future__ import annotations
+
+from gemseo import configure_logger
+from gemseo.algos.design_space import DesignSpace
+from gemseo.algos.parameter_space import ParameterSpace
+from gemseo.disciplines.analytic import AnalyticDiscipline
+
+from gemseo_umdo.scenarios.umdo_scenario import UMDOScenario
+
+configure_logger()
+
+# %%
+# Firstly,
+# we create one discipline per couping equation
+# and a system discipline to compute the objective and constraints:
+system = AnalyticDiscipline({
+    "obj": "x**2 + z2 + y1**2 + exp(-y2)",
+    "c1": "3.16 - y1 ** 2",
+    "c2": "y2 - 24.0",
+})
+disc1 = AnalyticDiscipline({"y1": "(z1**2 + z2 + x - a*y2)**0.5"})
+disc2 = AnalyticDiscipline({"y2": "2/10*log(1+exp(10*y1))-y1-2/10*log(2) + z1 + z2"})
+
+# %%
+# as well as the design space:
+design_space = DesignSpace()
+design_space.add_variable("x", 1, l_b=0.0, u_b=10.0, value=1.0)
+design_space.add_variable("z1", 1, l_b=-10.0, u_b=10.0, value=4.0)
+design_space.add_variable("z2", 1, l_b=0.0, u_b=10.0, value=3.0)
+
+# %%
+# Secondly,
+# we define the uncertain space:
+uncertain_space = ParameterSpace()
+# %%
+# with an uncertainty over the constant `"a"`:
+uncertain_space.add_random_variable(
+    "a", "OTTriangularDistribution", minimum=0.1, maximum=0.3, mode=0.2
+)
+
+# %%
+# Then,
+# we define a [UMDOScenario][gemseo_umdo.scenarios.umdo_scenario.UMDOScenario]
+# to minimize the statistic $\mathbb{E}[f(x,z_2,y_1,y_2)]$
+# estimated using a control variates technique
+# based on Taylor polynomials and 50 samples at each iteration of the optimization loop:
+scenario = UMDOScenario(
+    [system, disc1, disc2],
+    "MDF",
+    "obj",
+    design_space,
+    uncertain_space,
+    "Mean",
+    statistic_estimation="ControlVariate",
+    statistic_estimation_parameters={"n_samples": 100},
+)
+
+# %%
+# while satisfying the constraints
+# $\mathbb{E}[c_1(y_1)]+3\mathbb{S}[c_1(y_1)] \leq 0$
+# and
+# $\mathbb{E}[c_2(y_2)]+3\mathbb{S}[c_2(y_2)] \leq 0$:
+scenario.add_constraint("c1", "Margin", factor=3.0)
+scenario.add_constraint("c2", "Margin", factor=3.0)
+
+# %%
+# We execute this scenario using the gradient-free optimizer COBYLA:
+scenario.execute({"algo": "NLOPT_COBYLA", "max_iter": 200})
+
+# %%
+# and plot the optimization history:
+scenario.post_process("OptHistoryView", save=True, show=False)
+
+# %%
+# Lastly,
+# we can compare the numerical solution of this Sellar's MDO problem under uncertainty
+result = scenario.optimization_result
+(result.x_opt, result.constraint_values, result.f_opt)
+
+# %%
+# to the solution of the Sellar's MDO problem without uncertainty,
+# namely
+# $(x^*,c^*,f^*)=([0, 1.77, 0], [0, -20.58], 3.19)$.
---
+++ public/develop/generated/examples/umdo/control_variate/plot_cv_rosenbrock.py
@@ -0,0 +1,96 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This work is licensed under a BSD 0-Clause License.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+# FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+r"""# The Rosenbrock mono-disciplinary problem
+
+In this example, we consider the Rosenbrock mono-disciplinary optimization problem
+
+$$\min_{x,y\in[-2,2]} \mathbb{E}[(U-x)^2+100(y-x^2)^2]$$
+
+where $U\sim\mathcal{N}(0,0.0025)$ is a Gaussian variable and $\mathbb{E}$ is the
+expectation operator.
+
+In the following, we will call $f$ the function computing $(U-x)^2+100(y-x^2)^2$ given
+$x$, $y$ and $U$.
+"""
+
+from __future__ import annotations
+
+from gemseo import configure_logger
+from gemseo.algos.design_space import DesignSpace
+from gemseo.algos.parameter_space import ParameterSpace
+from gemseo.disciplines.analytic import AnalyticDiscipline
+
+from gemseo_umdo.scenarios.umdo_scenario import UMDOScenario
+
+configure_logger()
+
+# %%
+# Firstly,
+# we define the discipline implementing the Rosenbrock function $f$:
+discipline = AnalyticDiscipline({"z": "(u-x)**2+100*(y-x**2)**2"}, name="Rosenbrock")
+
+# %%
+# where $x,y$ belongs to the interval $[-2,2]$:
+design_space = DesignSpace()
+design_space.add_variable("x", l_b=-2, u_b=2.0, value=-2.0)
+design_space.add_variable("y", l_b=-2, u_b=2.0, value=-2.0)
+
+# %%
+# and $U$ is a Gaussian variable with unit mean
+# and standard deviation equal to 0.05:
+uncertain_space = ParameterSpace()
+uncertain_space.add_random_variable("u", "OTNormalDistribution", mu=1.0, sigma=0.05)
+
+# %%
+# Then,
+# we define a [UMDOScenario][gemseo_umdo.scenarios.umdo_scenario.UMDOScenario]
+# to minimize the statistic $\mathbb{E}[(U-x)^2+100(y-x^2)^2]$
+# estimated using a control variates technique
+# based on Taylor polynomials and 50 samples at each iteration of the optimization loop:
+scenario = UMDOScenario(
+    [discipline],
+    "DisciplinaryOpt",
+    "z",
+    design_space,
+    uncertain_space,
+    "Mean",
+    statistic_estimation="ControlVariate",
+    statistic_estimation_parameters={"n_samples": 30},
+)
+
+# %%
+# We execute it with the gradient-based optimizer SLSQP:
+#
+# !!! warning
+#     The implementation of statistic estimators do not allow for the moment
+#     to use analytical derivatives.
+#     Please use finite differences or complex step to approximate the gradients.
+scenario.set_differentiation_method("finite_differences")
+scenario.execute({"algo": "NLOPT_SLSQP", "max_iter": 100})
+
+# %%
+# and plot the optimization history:
+scenario.post_process("OptHistoryView", save=False, show=True)
+
+# %%
+# Lastly,
+# we can compare the numerical solution of this Rosenbrock problem under uncertainty
+(scenario.optimization_result.x_opt, scenario.optimization_result.f_opt)
+
+# %%
+# to the solution of the Rosenbrock problem without uncertainty,
+# namely
+# $(x^*,f^*)=([1, 1], 0)$.
