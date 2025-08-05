5b93bafdee00b5bc7c17f79b3b8a918c85d6fd59
AntoineD <>
2025-08-05 09:19:33 +0000

    Deployed b9236e4 to develop in public with MkDocs 1.6.1 and mike 2.1.3

--- public/develop/gen_credits.py
+++ public/develop/gen_credits.py
@@ -19,23 +19,21 @@
 
 from __future__ import annotations
 
-import operator
 import os
-import re
 import sys
-from importlib.metadata import PackageNotFoundError
-from importlib.metadata import metadata
+from collections import defaultdict
+from collections.abc import Iterable
+from importlib.metadata import distributions
 from itertools import chain
 from pathlib import Path
-from typing import TYPE_CHECKING
+from textwrap import dedent
+from typing import Union
 
-import requests
 from jinja2 import StrictUndefined
 from jinja2.sandbox import SandboxedEnvironment
+from packaging.requirements import Requirement
 
-if TYPE_CHECKING:
-    from collections.abc import Mapping
-
+# YORE: EOL 3.10: Replace block with line 2.
 if sys.version_info >= (3, 11):
     import tomllib
 else:
@@ -46,102 +44,176 @@ with project_dir.joinpath("pyproject.toml").open("rb") as pyproject_file:
     pyproject = tomllib.load(pyproject_file)
 project = pyproject["project"]
 project_name = project["name"]
-regex = re.compile(r"(?P<dist>[\w.-]+)(?P<spec>.*)$")
+devdeps = [
+    dep
+    for group in pyproject["dependency-groups"].values()
+    for dep in group
+    if not dep.startswith("-e")
+]
+
+PackageMetadata = dict[str, Union[str, Iterable[str]]]
+Metadata = dict[str, PackageMetadata]
 
 
-def _get_license(pkg_name: str) -> str:
+def _merge_fields(metadata: dict) -> PackageMetadata:
+    fields = defaultdict(list)
+    for header, value in metadata.items():
+        fields[header.lower()].append(value.strip())
+    return {
+        field: value
+        if len(value) > 1 or field in ("classifier", "requires-dist")
+        else value[0]
+        for field, value in fields.items()
+    }
+
+
+def _norm_name(name: str) -> str:
+    return name.replace("_", "-").replace(".", "-").lower()
+
+
+def _requirements(deps: list[str]) -> dict[str, Requirement]:
+    return {_norm_name((req := Requirement(dep)).name): req for dep in deps}
+
+
+def _extra_marker(req: Requirement) -> str | None:
+    if not req.marker:
+        return None
     try:
-        data = metadata(pkg_name)
-    except PackageNotFoundError:
-        # Typically for the test dependencies that are not installed
-        # in the doc environment.
-        pass
-    else:
-        license_name = data.get("License", "").strip()
-
-        # Do not use the full license text (with line returns).
-        if license_name and "\n" not in license_name and license_name != "UNKNOWN":
-            return license_name
-
-        # Look for the classifier.
-        for header, value in data.items():
-            if header == "Classifier" and value.startswith("License ::"):
-                license_name = value.rsplit("::", 1)[1].strip()
-                break
-
-        if license_name:
-            return license_name
-
-    # Look for the classifier from PyPI.
-    response = requests.get(f"https://pypi.org/pypi/{pkg_name}/json").json()
-    for value in response["info"]["classifiers"]:
-        if value.startswith("License ::"):
-            return value.rsplit("::", 1)[1].strip()
-
-    return "?"
+        return next(
+            marker[2].value
+            for marker in req.marker._markers
+            if getattr(marker[0], "value", None) == "extra"
+        )
+    except StopIteration:
+        return None
 
 
-def _get_deps(base_deps: Mapping[str, Mapping[str, str]]) -> dict[str, dict[str, str]]:
+def _get_metadata() -> Metadata:
+    metadata = {}
+    for pkg in distributions():
+        name = _norm_name(pkg.name)  # type: ignore[attr-defined,unused-ignore]
+        metadata[name] = _merge_fields(pkg.metadata)  # type: ignore[arg-type]
+        metadata[name]["spec"] = set()
+        metadata[name]["extras"] = set()
+        metadata[name].setdefault("summary", "")
+        _set_license(metadata[name])
+    return metadata
+
+
+def _set_license(metadata: PackageMetadata) -> None:
+    license_field = metadata.get("license-expression", metadata.get("license", ""))
+    license_name = (
+        license_field if isinstance(license_field, str) else " + ".join(license_field)
+    )
+    check_classifiers = license_name in (
+        "UNKNOWN",
+        "Dual License",
+        "",
+    ) or license_name.count("\n")
+    if check_classifiers:
+        license_names = [
+            classifier.rsplit("::", 1)[1].strip()
+            for classifier in metadata["classifier"]
+            if classifier.startswith("License ::")
+        ]
+        license_name = " + ".join(license_names)
+    metadata["license"] = license_name or "?"
+
+
+def _get_deps(base_deps: dict[str, Requirement], metadata: Metadata) -> Metadata:
     deps = {}
-    for dep in base_deps:
-        parsed = regex.match(dep).groupdict()
-        dep_name = parsed["dist"].lower()
-        deps[dep_name] = {
-            "license": _get_license(dep_name),
-            "url": f"https://pypi.org/project/{dep_name}/",
-        }
+    for dep_name, dep_req in base_deps.items():
+        if dep_name not in metadata or dep_name == "griffe":
+            continue
+        metadata[dep_name]["spec"] |= {str(spec) for spec in dep_req.specifier}  # type: ignore[operator]
+        metadata[dep_name]["extras"] |= dep_req.extras  # type: ignore[operator]
+        deps[dep_name] = metadata[dep_name]
+
+    again = True
+    while again:
+        again = False
+        for pkg_name in metadata:
+            if pkg_name in deps:
+                for pkg_dependency in metadata[pkg_name].get("requires-dist", []):
+                    requirement = Requirement(pkg_dependency)
+                    dep_name = _norm_name(requirement.name)
+                    extra_marker = _extra_marker(requirement)
+                    if (
+                        dep_name in metadata
+                        and dep_name not in deps
+                        and dep_name != project["name"]
+                        and (
+                            not extra_marker or extra_marker in deps[pkg_name]["extras"]
+                        )
+                    ):
+                        metadata[dep_name]["spec"] |= {
+                            str(spec) for spec in requirement.specifier
+                        }  # type: ignore[operator]
+                        deps[dep_name] = metadata[dep_name]
+                        again = True
+
     return deps
 
 
 def _render_credits() -> str:
-    external_dependencies = _get_deps(project["dependencies"])
-    external_applications = _get_deps(
-        chain(
-            *project["optional-dependencies"].values(),
-            (
-                "commitizen",
-                "pre-commit",
-                "ruff",
-                "setuptools",
-                "setuptools-scm",
+    metadata = _get_metadata()
+    dev_dependencies = _get_deps(_requirements(devdeps), metadata)
+    prod_dependencies = _get_deps(
+        _requirements(
+            chain(  # type: ignore[arg-type]
+                project.get("dependencies", []),
+                chain(*project.get("optional-dependencies", {}).values()),
             ),
-        )
+        ),
+        metadata,
     )
-
-    # Add softwares not in PyPI.
-    external_applications.update({
-        "insert-license": {
-            "url": "https://github.com/Lucas-C/pre-commit-hooks",
-            "license": "MIT",
-        },
-        "pygrep-hooks": {
-            "url": "https://github.com/pre-commit/pygrep-hooks",
-            "license": "MIT",
-        },
-    })
-    external_dependencies["Python"] = {
-        "url": "https://python.org",
-        "license": "Python Software License",
-    }
-
-    # Add name for later sorting and rendering.
-    for deps in (external_dependencies, external_applications):
-        for name, dep in deps.items():
-            dep["name"] = name
 
     template_data = {
         "project_name": project_name,
-        "external_dependencies": sorted(
-            external_dependencies.values(), key=operator.itemgetter("name")
+        "prod_dependencies": sorted(
+            prod_dependencies.values(), key=lambda dep: str(dep["name"]).lower()
         ),
-        "external_applications": sorted(
-            external_applications.values(), key=operator.itemgetter("name")
+        "dev_dependencies": sorted(
+            dev_dependencies.values(), key=lambda dep: str(dep["name"]).lower()
         ),
     }
+    template_text = dedent(
+        """
+        # Credits
 
+        These projects were used to build *{{ project_name }}*. **Thank you!**
+
+        [Python](https://www.python.org/) |
+        [uv](https://github.com/astral-sh/uv)
+
+        {% macro dep_line(dep) -%}
+        [{{ dep.name }}](https://pypi.org/project/{{ dep.name }}/) | {{ dep.summary }} | {{ ("`" ~ dep.spec|sort(reverse=True)|join(", ") ~ "`") if dep.spec else "" }} | `{{ dep.version }}` | {{ dep.license }}
+        {%- endmacro %}
+
+        {% if prod_dependencies -%}
+        ### Runtime dependencies
+
+        Project | Summary | Version (accepted) | Version (last resolved) | License
+        ------- | ------- | ------------------ | ----------------------- | -------
+        {% for dep in prod_dependencies -%}
+        {{ dep_line(dep) }}
+        {% endfor %}
+
+        {% endif -%}
+        {% if dev_dependencies -%}
+        ### Development dependencies
+
+        Project | Summary | Version (accepted) | Version (last resolved) | License
+        ------- | ------- | ------------------ | ----------------------- | -------
+        {% for dep in dev_dependencies -%}
+        {{ dep_line(dep) }}
+        {% endfor %}
+
+        {% endif -%}
+        """,
+    )
     jinja_env = SandboxedEnvironment(undefined=StrictUndefined)
-    template_text = Path(project_dir / "docs" / "credits.md.jinja").read_text()
     return jinja_env.from_string(template_text).render(**template_data)
 
 
-print(_render_credits())  # noqa: T201
+print(_render_credits())
