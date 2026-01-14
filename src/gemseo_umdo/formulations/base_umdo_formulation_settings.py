ff721f95c1a712a1edf264b752e69be66d53fc5e
CI <group_11547342_bot_857959eb1dea5ce3d5541af3d12d09a3@noreply.gitlab.com>
2026-01-14 08:13:34 +0000

    Update dependencies

--- src/gemseo_umdo/formulations/base_umdo_formulation_settings.py
+++ src/gemseo_umdo/formulations/base_umdo_formulation_settings.py
@@ -14,6 +14,8 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """The base class for the settings of U-MDO formulations."""
 
+from __future__ import annotations
+
 from typing import ClassVar
 
 from pydantic import BaseModel
